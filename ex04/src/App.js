38e7ff26ac8acb7e76eea67ca436c414d69148ff
basslines <admir.osmanovic@gmail.com>
2021-04-02 23:03:33 +0200

    dodao projekat

---
+++ ex04/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import GetRequestSetHeaders from "./GetRequestSetHeaders";
+import "./GetRequestSetHeaders";
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+
+export default App;
