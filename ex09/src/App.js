c616fea45f01c58648b12328f997b92c24678108
basslines <admir.osmanovic@gmail.com>
2021-04-03 00:20:08 +0200

    dodao projekat

---
+++ ex09/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PutRequest from "./PutRequest";
+import "./PutRequest";
+
+function App() {
+  return (
+    <div className="App">
+      <PutRequest />
+    </div>
+  );
+}
+
+export default App;
