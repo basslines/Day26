9db20f00224faf161358bc6c7e5faa0f44727561
basslines <admir.osmanovic@gmail.com>
2021-04-02 23:33:36 +0200

    dodao projekat

---
+++ ex06/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PostRequestAsyncAwait from "./PostRequestAsyncAwait";
+import "./PostRequestAsyncAwait";
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
